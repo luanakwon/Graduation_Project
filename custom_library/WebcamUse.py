cb64ebfe391de389bd4ebcf4bee2ab52a37c9f6a
luanakwon <junnyboy99@gmail.com>
2022-01-01 04:36:57 -0800

    mediapipe hand not working so well

--- custom_library/WebcamUse.py
+++ custom_library/WebcamUse.py
@@ -13,6 +13,34 @@ def DrawSquare(src,center, size, color, thickness=1):
     dst = cv2.line(dst,(cx-s,cy+s),(cx-s,cy-s),color,thickness)
     return dst
 
+def DrawTiltedSquare(src,center,dir,size,color,thickness=1):
+    dv = dir.astype(np.float32).copy()
+    dv /= np.sqrt(np.sum(dv*dv))
+    dv = (dv*size).astype(np.int32)
+    Hdv = np.flip(dv).copy()
+    Hdv[1] *= -1
+    start = center - dv//2 - Hdv//2
+    dst = cv2.line(src,start,start+dv,color,thickness)
+    dst = cv2.line(dst,start+dv,start+dv+Hdv,color,thickness)
+    dst = cv2.line(dst,start+dv+Hdv,start+Hdv,color,thickness)
+    dst = cv2.line(dst,start+Hdv,start,color,thickness)
+    return dst
+
+def CropTiltedSquare(src,center,dir,size,dsize):
+    dv = dir.astype(np.float32).copy()
+    dv /= np.sqrt(np.sum(dv*dv))
+    dv = (dv*size).astype(np.int32)
+    Hdv = np.flip(dv).copy()
+    Hdv[1] *= -1
+    start = center - dv//2 - Hdv//2
+    s = dsize
+    input_pts = np.float32([start,start+dv,start+dv+Hdv,start+Hdv]).reshape(4,2)
+    output_pts = np.float32([[0,0],[s-1,0],[s-1,s-1],[0,s-1]])
+    M = cv2.getPerspectiveTransform(input_pts,output_pts)
+    dst = cv2.warpPerspective(src,M,(s,s),flags=cv2.INTER_LINEAR)
+    return dst
+
+
 def WebcamDemo():
     # CardDetection settings
     img_size = (480,640) # W,H
@@ -27,9 +55,9 @@ def WebcamDemo():
     area0 = 0 # pseudo area of last fram
     area_base = img_size[0]*img_size[1]*(p_c**2) # area of center grid cell(for norm)
     frame_counter = 0
-    area_diff_thres = 0.1 # heuristic value
+    area_diff_thres = 0.2 # heuristic value
     frames_to_use = np.zeros((100,img_size[1],img_size[0],3),dtype=np.uint8)
-    corners_to_use = np.zeros((100,4,2))
+    corners_to_use = np.zeros((100,4,2),dtype=np.float32)
     FDD_rets = np.zeros((100),dtype=np.uint8)
     FDD_dips = np.zeros((100,4,2),dtype=np.int32)
     FDD_dirs = np.zeros((100,4,2))
@@ -56,10 +84,12 @@ def WebcamDemo():
             break
 
         # mirror the frame
-        img = cv2.flip(frame,flipCode=1)
+        original_img = cv2.flip(frame,flipCode=1)
 
         # change to portrait of ratio 800:600
-        img = cv2.resize(img[:,140:500],dsize=img_size)
+        original_img = cv2.resize(original_img[:,140:500],dsize=img_size)
+        
+        img = original_img.copy()
 
         # all calculation in 1 channel grayscale img
         grimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -90,13 +120,14 @@ def WebcamDemo():
         if not ret or abs(area1-area0) > area_diff_thres:
             frame_counter = -1
         else:
-            frames_to_use[frame_counter] = img
+            frames_to_use[frame_counter] = original_img
             rgb_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
             rgb_img.flags.writeable = False
             ret,dip,dir = FDDtector.run(rgb_img)
             FDD_rets[frame_counter] = ret
             FDD_dips[frame_counter] = dip
             FDD_dirs[frame_counter] = dir
+            corners_to_use[frame_counter] = np.float32(corners).reshape((4,2))
 
         area0 = area1
         
@@ -116,15 +147,18 @@ def WebcamDemo():
     # display frames to use
     print("presskey")
     cv2.waitKey()
+    frame_counter = 0
     for i in range(frames_to_use.shape[0]):
-        frame = frames_to_use[i]
+        frame = frames_to_use[i].copy()
         if FDD_rets[i]:
             for dip, dir in zip(FDD_dips[i],FDD_dirs[i]):
                 frame = DrawSquare(frame,dip,100,(0,0,255),1)
                 frame = cv2.line(frame,dip,dip+dir.astype(np.int32),(255,0,0),1)
+                frame_counter = i
         cv2.imshow("frames_to_use",frame)
         cv2.waitKey(66)
 
+    print(frame_counter)
     # save frames to use as mp4 video
     vid_name = input("save video as (type q to quit)")
     if vid_name != 'q':
@@ -138,8 +172,22 @@ def WebcamDemo():
     # I think we'll use 1 frame instead of 100
     # we will finely adjust the corners,
     # but not implemented in this demo
-    img = frames_to_use[-1]
-    input_pts = np.array(corners,dtype=np.float32).reshape((4,2))
+
+    # seems like mediapipe hand detection doesn't work very well
+    # with palm covered. Since it tends to shrink the hands, choose
+    # the frame with finger dips spreaded wide
+    max_wide = 0
+    for i in range(frames_to_use.shape[0]):
+        if FDD_rets[i]:
+            wideness = (-FDD_dips[i][0][0]-FDD_dips[i][1][0]
+                +FDD_dips[i][2][0]+FDD_dips[i][3][0])
+            if max_wide < wideness:
+                frame_counter = i
+                max_wide = wideness
+
+    # warp perspective
+    img = frames_to_use[frame_counter]
+    input_pts = corners_to_use[frame_counter]
     output_pts = np.array(
         [crop_window[0][1],crop_window[0][0],
         crop_window[1][1],crop_window[0][0],
@@ -148,9 +196,30 @@ def WebcamDemo():
     print(input_pts, output_pts)
     M=cv2.getPerspectiveTransform(input_pts,output_pts)
     T_img = cv2.warpPerspective(img,M,img_size,flags=cv2.INTER_LINEAR)
+    # transfrom 8 FDD points
+    FDD_cors = np.ones((3,8))
+    FDD_cors[:2,:4] = np.transpose(FDD_dips[frame_counter])
+    FDD_cors[:2,4:] = np.transpose(FDD_dirs[frame_counter])
+    FDD_cors = np.matmul(M,FDD_cors)
+    FDD_cors = (FDD_cors/FDD_cors[2]).astype(np.int32)
+    FDD_cors = np.transpose(FDD_cors[:2])
+
+    # crop and save 4 finger dips
+    ROI_crops = np.zeros((4,200,200,3),dtype=np.uint8)
+    for i, (dip, dir) in enumerate(zip(FDD_cors[:4],FDD_cors[4:])):
+        ROI_crops[i] = CropTiltedSquare(T_img,dip,dir,100,200)
+
+    # draw indicators
+    for dip, dir in zip(FDD_cors[:4],FDD_cors[4:]):
+        T_img = DrawTiltedSquare(T_img,dip,dir,100,(0,255,0),1)
+        T_img = cv2.line(T_img,dip,dip+dir,(255,255,0),1)
+
 
     cv2.imshow('Transfrormed img',T_img)
     cv2.waitKey()
+    for i, crop in enumerate(ROI_crops):
+        cv2.imshow(f'crop{i}',crop)
+        cv2.waitKey()
     cv2.imwrite('shba_hand.png',T_img)
 
 
--- custom_library/nakseo.py
+++ custom_library/nakseo.py
@@ -1,64 +1,26 @@
 import cv2
 import numpy as np
 import mediapipe as mp
+import time
 
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+img = cv2.imread('shba_hand.png')
+t0 = time.time()
+input_pts = np.float32([[200,100],[100,200],[200,300],[300,200]])
+output_pts = np.float32([[0,0],[0,200],[200,200],[200,0]])
+M = cv2.getPerspectiveTransform(input_pts,output_pts)
+img2 = cv2.warpPerspective(img,M,(201,201),flags=cv2.INTER_LINEAR)
+print(f'big input {time.time()-t0}')
 
-cap = cv2.VideoCapture(0)
+t0 = time.time()
+img_small = img[100:300,100:300]
+input_pts -= 100
+M = cv2.getPerspectiveTransform(input_pts,output_pts)
+img3 = cv2.warpPerspective(img_small,M,(201,201),flags=cv2.INTER_LINEAR)
+print(f'small input {time.time()-t0}')
 
-resolution = (480,640)
 
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    model_complexity= 0,
-    min_detection_confidence=0.5,) as hands:
-    
-    while cv2.waitKey(33) < 0:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # mirror the frame
-        img = cv2.flip(frame,flipCode=1)
-
-        # change to portrait of ratio 800:600
-        img = cv2.resize(img[:,140:500],dsize=resolution)
-
-        img.flags.writeable = False
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = hands.process(img)
-
-        img.flags.writeable = True
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    img,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
-
-            for handedness in results.multi_handedness:
-                print(handedness)
-            
-        #         for p_idx in [6,10,14,18]:
-        #             print(hand_landmarks.landmark[p_idx])
-        #             anc_x = hand_landmarks.landmark[p_idx].x
-        #             anc_y = hand_landmarks.landmark[p_idx].y
-        #             anc_x = int(anc_x*resolution[0]-ROI_win_size/2)
-        #             anc_y = int(anc_y*resolution[1]-ROI_win_size/2)
-        #             ROI_mask[anc_y:anc_y+ROI_win_size,anc_x:anc_x+ROI_win_size] = 1
-        
-        # masked_img = img*ROI_mask
-
-        #img = cv2.flip(img, 1)
-        cv2.resize(img,dsize=(0,0),fx=0.5,fy=0.5)
-        cv2.imshow('title',img)
-
-    
-    
+cv2.imshow('original',img)
+cv2.imshow('tilted',img2)
+cv2.imshow('tilted2',img3)
+cv2.waitKey()
